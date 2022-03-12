2611b016df460c4c17f867132803c56a616e8cba
Jose Camacho <jose.camacho@alter-info.com>
2022-03-12 16:46:43 -0400

    Fix port

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'process.env.DB_HOST',
-      port: process.env.DB_PORT,
-      username: 'process.env.DB_USER',
-      password: 'process.env.DB_PASS',
-      database: 'process.env.DB_NAME',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [ Category, Product ],
       synchronize: true
     }),
