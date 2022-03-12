5c3a284486326da4e0bcde606471cd5230c84e1a
Jose Camacho <jose.camacho@alter-info.com>
2022-03-12 16:42:07 -0400

    env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'pizzeria-donremolo',
+      host: 'process.env.DB_HOST',
+      port: process.env.DB_PORT,
+      username: 'process.env.DB_USER',
+      password: 'process.env.DB_PASS',
+      database: 'process.env.DB_NAME',
       entities: [ Category, Product ],
       synchronize: true
     }),
